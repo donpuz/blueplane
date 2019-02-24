b91cc3ee57048065cdff657d3cfc6700359fb944
knot-eye <ryanzhu.1@outlook.com>
2019-02-24 17:48:36 -0500

    1.1

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ $("button").click(function() {
 });
 var data1;
 Papa.parse("aircraftDatabase.csv", {
-  delimeter: ", ",
+  delimeter: "",
   header:true,
   complete:function(results) {
     data1 = results;
@@ -20,7 +20,7 @@ Papa.parse("aircraftDatabase.csv", {
 
 var lat2 = 0;
 var lon2 = 0;
-var changed = document.getElementById("airplanes");
+var changed = document.getElementById("testio");
 function showPosition(position) {
     x.innerHTML = "Latitude: " + position.coords.latitude +
     "<br>Longitude: " + position.coords.longitude;
@@ -28,35 +28,21 @@ function showPosition(position) {
     lon2 = position.coords.longitude;
   }
 
-function initMap() {
-  var myLatLng = {lat: 42.3605297, lng: -71.0871475};
-  console.log(myLatLng);
-  var map = new google.maps.Map(document.getElementById('map'), {
-  zoom: 4,
-    center: myLatLng
-  });
-
-  var marker = new google.maps.Marker({
-    position: myLatLng,
-    map: map,
-    title: 'You Are Here'
-  });
-}
-function addMarker(location) {
-  var marker = new google.maps.Marker({
-    position: location,
-    map: map,
-  });
-}
 
 var airplanes = [];
 var request = new XMLHttpRequest();
 request.open('GET', 'https://opensky-network.org/api/states/all', true);
+var coords = [[]];
+var alla = [];
+var allo = [];
+var cords = [];
 request.onload = function() {
 var data = JSON.parse(this.response);
 //var data = this.response;
-for(var i = 0; i < Object.keys(data.states).length; i++ ){
+var count = 0;
 
+
+for(var i = 0; i < Object.keys(data.states).length; i++ ){
   /*
   var carriers = [[]];
   if (carriers.indexOf(String(data.states[i][1])) == -1) {
@@ -69,11 +55,8 @@ for(var i = 0; i < Object.keys(data.states).length; i++ ){
   }
   */
 
-
 var lon1 = parseFloat(data.states[i][5]);
 var lat1 = parseFloat(data.states[i][6]);
-
-var pos = {lat:String(lat1), lon:String(lon1)};
 
 Number.prototype.toRad = function() {
  return this * Math.PI / 180;
@@ -93,12 +76,67 @@ if (d < 20) {
   airplanes.push(data.states[i][1].trim());
   console.log(data.states[i][1].trim);
   console.log(airplanes);
-  console.log(data1);
+  alla.push(data.states[i][6]);
+  allo.push(data.states[i][5]);
+  console.log(coords);
+  var add = airplanes[count];
+  count+=1;
+  $("p").append(function(){
+     return "<b>The airplane " + String(add) + " is close to you.</b><br> In fact, it is about " + String(Math.round(d)) + " kilometers from you, if you want real numbers it's " + String(d) + " kilometers.<br>";
+
+});
 
 //  addMarker(pos);
 
 }
 
 }}
+function initMap() {
+  var idunno = [
+    {
+      position: new google.maps.LatLng(alla[0], allo[0]),
+    },
+    {
+      position: new google.maps.LatLng(alla[1], allo[1]),
+    },
+    {
+      position: new google.maps.LatLng(alla[2], allo[2]),
+    }
+
+  ]
+  var test = 42.3605297
+  var myLatLng = {lat: test, lng: -71.0871475};
+  console.log(myLatLng);
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 4,
+    center: myLatLng
+  });
+  idunno.forEach(function(feature) {
+    var marker = new google.maps.Marker({
+      position: feature.position,
+      map: map
+    });
+  });
+  /*
+  for (var i = 0; i < coords.length; i++) {
+    var ll2 = {lat: parseFloat(alla[i]), lng: parseFloat(allo[i])}
+    //var latLng = new google.maps.LatLng(coords[i]);
+    console.log(ll2);
+    var marker = new google.maps.Marker({
+      position: ll2,
+      map:map
+    })
+  }
+*/
+  var marker = new google.maps.Marker({
+    position: myLatLng,
+    map: map,
+    title: 'You Are Here'
+
+  });
+
+
+}
+
 
 request.send();
